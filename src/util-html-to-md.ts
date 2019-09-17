356354fe805c99e244311a24d80314dd4cec5211
fartinmartin <30564420+fartinmartin@users.noreply.github.com>
2019-09-16 21:41:18 -0700

    Return node if img tagName is not found
    This allows for HTML elements and custom components to be added to markdown.

--- src/util-html-to-md.ts
+++ src/util-html-to-md.ts
@@ -6,6 +6,7 @@ export const toMdNode = (node: RemarkNode): RemarkNode | null => {
   const value = node.value
   const parsed = parse5.parseFragment(value)
   const imgNode = parsed.childNodes.find(node => node.tagName === 'img')
+  if (!imgNode) {return node}
   const attrs = imgNode.attrs.reduce((acc, cur) => {
     const { name, value } = cur
     acc[name] = value
