3b6f0e29ba983e1c9de8c50195c49e80fd94c745
fartinmartin <30564420+fartinmartin@users.noreply.github.com>
2019-09-16 22:05:02 -0700

    Pass on non 'img' nodes
    This allows for HTML elements and custom components to be added to markdown.

--- src/util-html-to-md.ts
+++ src/util-html-to-md.ts
@@ -6,7 +6,10 @@ export const toMdNode = (node: RemarkNode): RemarkNode | null => {
   const value = node.value
   const parsed = parse5.parseFragment(value)
   const imgNode = parsed.childNodes.find(node => node.tagName === 'img')
-  if (!imgNode) {return node}
+  
+  // not an img? don't touch it
+  if (!imgNode) return null
+  
   const attrs = imgNode.attrs.reduce((acc, cur) => {
     const { name, value } = cur
     acc[name] = value
