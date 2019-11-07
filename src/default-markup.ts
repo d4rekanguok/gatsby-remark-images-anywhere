7e5161722eee9ce940ee48ea9166fec58a5bc2cd
Derek Nguyen <derek@penandpillow.com>
2019-11-08 00:28:42 +0900

    Feat/markup (#15)
    * Improved defaultMarkup (#9)
    
    * Beefed up + moved defaultMarkup to external file.
    
    * 😕
    
    * Tidying up.
    
    * Beefed up + moved defaultMarkup to external file.
    
    * Fixed blur style syntax error.
    
    * Add (brittle) logic to check type of sharp process
    
    * Update CSS for Safari.
    
    * Update sharpMethod logic.
    
    * 👎 logic in markup /
    👍 check props.sharpMethod
    
    * Better comments, better classname for placeholder.
    
    * Update html-to-md (?)
    
    * Delete .DS_Store
    
    * console.log if file is GIF or SVG (v useful, haha)
    
    * Added sharpMethod: fixed markup.
    
    * svgPlaceholder specific styles
    
    * .trim() to cleanup HTML strings.
    
    * Attempt at 1 level deeper for img node filtering.
    
    * Switch back to original html-to-md.ts
    
    * .trim() doesn't seem to do anything.
    
    * Switch back to original download-image.ts
    
    * add markup file to .npmignore, show comments only in development mode
    
    * pass options into createMarkup function, fix typings
    
    * pass in backgroundColor options
    
    * use startsWith instead of includes
    
    * remove noscrip block, add quote for loading attr
    
    * allow passing in custom wrapperstyle
    
    * show source image, take class name out
    
    * refactor css
    
    * refactor markup
    
    * touch ups
    
    * 1.1.0-beta.0

---
+++ src/default-markup.ts
@@ -0,0 +1,182 @@
+import { CreateMarkup, CreateMarkupArgs, MarkupOptions } from './type'
+import {
+  CLASS_WRAPPER,
+  CLASS_PADDING,
+  CLASS_LINK,
+  CLASS_IMAGE,
+  CLASS_PLACEHOLDER,
+} from './constants'
+
+const absoluteStyle = `
+  position: absolute;
+  top: 0; left: 0;
+  width: 100%;
+  height: 100%;
+`
+
+type F = (arg: string) => string
+type Pipe = (...fs: F[]) => F
+const pipe: Pipe = (...fs) => x => fs.reduce((v, f) => f(v), x)
+
+/**
+ * Only show comment during develop
+ */
+type Comment = (text: string) => string
+const comment: Comment = text => process.env.NODE_ENV !== 'production'
+  ? `<!--${text}-->`
+  : ``
+
+type HandleWrapperStyle = (wrapperStyle: string | Function, data: Object) => string
+const handleWrapperStyle: HandleWrapperStyle = (wrapperStyle, data) => {
+  if (typeof wrapperStyle === 'function') {
+    return wrapperStyle(data)
+  }
+  if (typeof wrapperStyle === 'string') {
+    return wrapperStyle
+  }
+  console.warn('[gatsby-remark-images-anywhere] wrapperStyle is expected to be either a string or a function.')
+}
+
+type ProcessMarkup = (data: CreateMarkupArgs, options: MarkupOptions, input: string) => string
+const processMainImage: ProcessMarkup = (data, options) => {
+  const { srcSet, src, alt } = data
+  const { loading } = options
+
+  return `
+    <picture>
+      <source srcset="${srcSet}">
+      <img
+        class="${CLASS_IMAGE}"
+        src="${src}"
+        srcset="${srcSet}"
+        title="${alt}"
+        alt="${alt}"
+        loading="${loading}"
+        style="
+          ${absoluteStyle}
+          object-fit: cover;
+          object-position: center center;"
+      >
+    </picture>
+  `
+}
+
+const processPlaceholder: ProcessMarkup = (data, options, input) => {
+  const { tracedSVG, base64 } = data
+  const { tracedSVG: enableSVG, blurUp, backgroundColor } = options
+
+  let placeholderData: null | string = null
+
+  if (enableSVG && tracedSVG) {
+    placeholderData = tracedSVG
+  }
+
+  if (blurUp && base64) {
+    placeholderData = base64
+  }
+
+  const markup = placeholderData
+    ? `
+      ${comment('show a placeholder image.')}
+      <div
+        class="${CLASS_PLACEHOLDER}"
+        style="
+          ${absoluteStyle}
+          background: ${backgroundColor} url(${placeholderData}) center / cover no-repeat;
+        "
+      ></div>
+    `
+    : ''
+  
+  return markup + input
+}
+
+const processWrapper: ProcessMarkup = (data, options, input) => {
+  const { sharpMethod, aspectRatio, width, height } = data
+  const { wrapperStyle } = options
+  const processedWrapperStyle = handleWrapperStyle(wrapperStyle, data)
+  
+  let markup = ''
+
+  if (sharpMethod === 'fluid') {
+    const styles = {
+      imageWrapper: `
+        position: relative;
+        overflow: hidden;
+        ${processedWrapperStyle}
+      `,
+      imagePadding: `
+        width: 100%;
+        padding-bottom: ${100 / aspectRatio}%;
+      `,
+    };
+
+    markup = `
+      <div class="${CLASS_WRAPPER}" style="${styles.imageWrapper}">
+      
+        ${comment('preserve the aspect ratio')}
+          <div class="${CLASS_PADDING}" style="${
+            styles.imagePadding
+          }"></div>
+        
+        ${input}
+      </div>
+    `
+  }
+
+  if (sharpMethod === 'fixed') {
+    const styles = `
+      position: relative;
+      overflow: hidden;
+      display: block;
+      width: ${width}px;
+      height: ${height}px;
+      ${processedWrapperStyle}
+    `
+
+    markup = `
+      <div class="${CLASS_WRAPPER}" style="${styles}">
+        ${input}
+      </div>
+    `
+  }
+
+  return markup
+}
+
+const processLinkToOriginal: ProcessMarkup = (data, options, input) => {
+  const { originalImg, src } = data
+  const { linkImagesToOriginal } = options
+
+  if (!linkImagesToOriginal) {
+    return input
+  }
+
+  // only fluid returns original image
+  return  `
+    <a 
+      class="${CLASS_LINK}"
+      target="_blank" 
+      rel="noopener"
+      style="display: block;"
+      href="${originalImg || src}"
+    >${input}
+    </a>`
+}
+
+export const defaultMarkup: CreateMarkup = (data, options) => {
+  if (!options) throw new Error('[gatsby-remark-images-anywhere] createMarkup: No options')
+  const mainImage = processMainImage.bind(null, data, options)
+  const placeholder = processPlaceholder.bind(null, data, options)
+  const wrapper = processWrapper.bind(null, data, options)
+  const link = processLinkToOriginal.bind(null, data, options)
+
+  const markup = pipe(
+    mainImage,
+    placeholder,
+    wrapper,
+    link,
+  )('')
+
+  return markup
+}
